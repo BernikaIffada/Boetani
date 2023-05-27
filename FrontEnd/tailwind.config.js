4358fdf89c60e6ae8cfe0d34b84918187911f9c6
rasy00 <rasyadamhar2001@gmail.com>
2023-05-28 06:20:09 +0700

    add tab

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/*.html", "./src/template/*.html"],
+  content: ["./src/*.html"],
   theme: {
     extend: {},
   },
