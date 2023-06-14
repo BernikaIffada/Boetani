3b6d3c66631d23e43f8737f6c53c5b36b9b0d761
BernikaIffada <bernikaiffada@gmail.com>
2023-06-15 01:25:13 +0700

    menyelesaikan add dan delete pertanyaan dengan menambah api login dan registrasi

--- Backend/application/controllers/Api.php
+++ Backend/application/controllers/Api.php
@@ -39,14 +39,14 @@ class Api extends RestController
     while ($id >= 0) {
       if ($this->hashId($id) === $hash) {
         break;
-      }else if($id >= 99999){
-       $notFound = true;
-       break;
+      } else if ($id >= 99999) {
+        $notFound = true;
+        break;
       }
       $id++;
     }
 
-    if($notFound){
+    if ($notFound) {
       throw new Exception("Not Found");
     }
 
@@ -64,7 +64,7 @@ class Api extends RestController
       array_push($pertanyaanIds, $p->id_pertanyaan);
 
       //   get user
-      $user = $this->getUserById($p->id_user);
+      $user = $this->getUserById($p->id);
       $p->user_name = $user->name;
       $pertanyaan[$index] = $p;
     }
@@ -120,31 +120,31 @@ class Api extends RestController
   {
 
     // getting id
-    try{
+    try {
       $id = $this->getIdByHash($hash);
-    }catch(Exception $e){
+    } catch (Exception $e) {
       return $this->response([
         "status" => "Fail",
         "error" => "true",
         "message" => "Not found",
       ], 404);
     }
-    
+
 
     // getting pertanyan by id
-    try{
+    try {
       $pertanyaan = $this->PertanyaanModel->pertanyaanById($id)[0];
-    }catch(Exception $e){
+    } catch (Exception $e) {
       return $this->response([
         "status" => "Fail",
         "error" => "true",
         "massege" => "Question not found",
       ], 404);
     }
-    
-    
+
+
     // get user
-    $user = $this->getUserById($pertanyaan->id_user);
+    $user = $this->getUserById($pertanyaan->id);
     $pertanyaan->user_name = $user->name;
 
     // getting jawaban by id_pertanyaan
@@ -155,7 +155,7 @@ class Api extends RestController
       $balasans = $this->BalasanModel->listByIdJawaban($jawaban->id_jawaban);
 
       // get user
-      $user = $this->getUserById($jawaban->id_user);
+      $user = $this->getUserById($jawaban->id);
       $jawaban->user_name = $user->name;
       $jawabans[$index] = $jawaban;
 
@@ -165,7 +165,7 @@ class Api extends RestController
       if (count($balasans) !== 0) {
         // get user
         foreach ($balasans as $index => $balasan) {
-          $user = $this->getUserById($balasan->id_user);
+          $user = $this->getUserById($balasan->id);
           $balasan->user_name = $user->name;
           $balasans[$index] = $balasan;
         }
@@ -242,13 +242,14 @@ class Api extends RestController
     if ($query) {
       if ($query === "add") {
         // get body request
-        $pertanyaan = $this->post("pertanyaan");
-
+        $pertanyaan = $this->post();
+        // print_r($pertanyaan);
+        // die;
         // validating
-        $wouldHave = ["judul","isi","id_user","id_kategori","image"] ;
+        $wouldHave = ["judul", "isi", "id", "id_kategori"];
         $isValidate = true;
         foreach ($wouldHave as $key) {
-          if(!array_key_exists($key,$pertanyaan)){
+          if (!array_key_exists($key, $pertanyaan)) {
             $response = [
               "data" => [
                 "status" => "Fail",
@@ -260,10 +261,25 @@ class Api extends RestController
             $isValidate = false;
           }
         }
-        
+        if (!empty($_FILES['image'])) {
+          $uploadedFile = $_FILES['image'];
+
+          // Access file properties
+          $tempFilePath = $uploadedFile['tmp_name'];
+
+          // Read the file contents
+          $fileContents = file_get_contents($tempFilePath);
+
+          // Convert the file contents to base64
+          $base64File = base64_encode($fileContents);
+
+          // Add the base64 encoded file to the $pertanyaan array
+          $base64_format = 'data:image/png;base64,';
+          $pertanyaan['image'] = $base64_format . $base64File;
+        }
         // storing
-        if($isValidate){
-          try{
+        if ($isValidate) {
+          try {
             $this->PertanyaanModel->add_pertanyaan($pertanyaan);
             $response = [
               "data" => [
@@ -273,7 +289,7 @@ class Api extends RestController
               ],
               "statusCode" => 201
             ];
-          }catch(Exception $e){
+          } catch (Exception $e) {
             $response = [
               "data" => [
                 "status" => "Fail",
@@ -290,8 +306,7 @@ class Api extends RestController
 
           // lakuin response
         }
-      */
-       else {
+      */ else {
         $response = [
           "data" => [
             "status" => "Fail",
@@ -319,18 +334,19 @@ class Api extends RestController
   }
 
   // update pertanyaan handler
-  public function pertanyaan_put(){
+  public function pertanyaan_put()
+  {
     $idHash = $this->get('id');
     $query = $this->query("action");
 
     if (!($idHash === null)) {
       // cek apakah hash nya berhasil di convert tidak 
       // jika ditemukan maka lanjutkan
-      try{
+      try {
         $id = $this->getIdByHash($idHash);
-        if($query){
-          if($query === "upvote"){
-            
+        if ($query) {
+          if ($query === "upvote") {
+
             $response = [
               "data" => [
                 "status" => "Success",
@@ -339,7 +355,7 @@ class Api extends RestController
               ],
               "statusCode" => 200
             ];
-          }else if($query === "downvote"){
+          } else if ($query === "downvote") {
 
             $response = [
               "data" => [
@@ -349,7 +365,7 @@ class Api extends RestController
               ],
               "statusCode" => 200
             ];
-          }else{
+          } else {
             $response = [
               "data" => [
                 "status" => "Fail",
@@ -359,7 +375,7 @@ class Api extends RestController
               "statusCode" => 400
             ];
           }
-        }else{
+        } else {
           $response = [
             "data" => [
               "status" => "Fail",
@@ -370,8 +386,8 @@ class Api extends RestController
           ];
         }
       }
-        // jika tidak jalankan ini
-      catch(Exception $e){
+      // jika tidak jalankan ini
+      catch (Exception $e) {
         $response = [
           "data" => [
             "status" => "Fail",
@@ -395,7 +411,123 @@ class Api extends RestController
     $this->response($response["data"], $response["statusCode"]);
   }
 
+  public function pertanyaan_delete()
+  {
+    $pertanyaan_id = $this->uri->segment(3);
+    try {
+      $id = $this->getIdByHash($pertanyaan_id);
+      $this->PertanyaanModel->delete_pertanyaan($id);
 
+      $response = [
+        "data" => [
+          "status" => "Success",
+          "error" => "false",
+          "message" => "Data has been deleted"
+        ],
+        "statusCode" => 200
+      ];
+      $this->response($response['data'], $response['statusCode']);
+    } catch (\Throwable $th) {
+      $response = [
+        "data" => [
+          "status" => "Fail",
+          "error" => "true",
+          "message" => "Data cannot be deleted"
+        ],
+        "statusCode" => 400
+      ];
+      $this->response($response['data'], $response['statusCode']);
+    }
+  }
+
+  public function login_post()
+  {
+    $email = $this->post('email');
+    $password = $this->post('password');
+
+    // Retrieve the user from the database based on the provided email
+    $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+    if (!$user) {
+      $this->response([
+        'status' => 'error',
+        'message' => 'Invalid credentials',
+        'data' => null
+      ], RestController::HTTP_UNAUTHORIZED);
+      return;
+    }
+
+    // Verify the password
+    if (!password_verify($password, $user['password'])) {
+      $this->response([
+        'status' => 'error',
+        'message' => 'Invalid credentials',
+        'data' => null
+      ], RestController::HTTP_UNAUTHORIZED);
+      return;
+    }
+
+    // Generate a token or session for authentication
+    // ...
+
+    $playload = [
+      'sub' => [
+        $user['id'],
+        $user['email'],
+      ],
+      'expiresIn' => strtotime('+1 day', time())
+    ];
+
+    $token = $this->generateToken($playload);
+
+    // Return a success response with the generated token or session
+    $this->response([
+      'status' => 'success',
+      'message' => 'Login successful',
+      'data' => ['token' => $token]
+    ], RestController::HTTP_OK);
+  }
+  private function generateToken($payload)
+  {
+    $header = json_encode(['alg' => 'HS256', 'typ' => 'JWT']);
+    $header = base64_encode($header);
+
+    $payload = json_encode($payload);
+    $payload = base64_encode($payload);
+
+    $key = '18726861283847128';
+
+    $signature = hash_hmac('sha256', "$header.$payload", $key, true);
+    $signature = base64_encode($signature);
+
+    $token = "$header.$payload.$signature";
+
+    return $token;
+  }
+
+  public function register_post()
+  {
+    $name = $this->post('name');
+    $email = $this->post('email');
+    $password = $this->post('password');
+
+    // Retrieve the user from the database based on the provided email
+    $data =
+      [
+        'name' => $name,
+        'email' => $email,
+        'password' => password_hash(
+          $password,
+          PASSWORD_DEFAULT
+        )
+      ];
+    $this->db->insert('user', $data);
+
+    $this->response([
+      'status' => 'success',
+      'message' => 'Registrasi successful',
+    ], RestController::HTTP_OK);
+  }
 }
 
 /*
@@ -405,4 +537,7 @@ class Api extends RestController
     localhost/boetani/api/user/id/1
     localhost/boetani/api/pertanyaan/id/<hash>?action=upvote
     localhost/boetani/api/pertanyaan?action=add
+    localhost/boetani/api/pertanyaan?action=delete
+    localhost/boetani/api/login
+    localhost/boetani/api/register
  */
