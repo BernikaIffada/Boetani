bbf185b31986dbb30632e44f666617562cf610f2
Bernika_Iffada <101461037+BernikaIffada@users.noreply.github.com>
2023-06-16 16:08:50 +0700

    Update JawabanModel.php
    modifikasi add dan delete jawaban

--- Backend/application/models/JawabanModel.php
+++ Backend/application/models/JawabanModel.php
@@ -1,53 +1,71 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
+// extends class Model
 class JawabanModel extends CI_Model
 {
-    protected $table = 'jawaban';
-    protected $primaryKey = 'id_jawaban';
+  public $id_pertanyaan;
+  public $isi;
+  public $id;
+  public $created_at;
+  public $updated_at;
+  public $upvote;
+  public $downvote;
 
-    protected $returnType = 'object';
-    protected $useSoftDeletes = false;
+  // function untuk insert data ke tabel jawaban
+  public function add_jawaban($jawaban)
+  {
+    // store to prop this model
+    $this->id_pertanyaan = $jawaban["id_pertanyaan"];
+    $this->isi = $jawaban["isi"];
+    $this->id = $jawaban["id_user"];
+    $this->upvote = 0;
+    $this->downvote = 0;
+    $this->created_at = date("Y-m-d", time());
+    $this->updated_at = date("Y-m-d", time());
+    $this->id_jawaban = $jawaban["id_jawaban"];
 
-    protected $allowedFields = ['id_pertanyaan', 'id', 'isi'];
-
-    protected $useTimestamps = true;
-    protected $createdField = 'created_at';
-    protected $updatedField = 'updated_at';
-    protected $deletedField = 'deleted_at';
-
-    protected $validationRules = [];
-    protected $validationMessages = [];
-    protected $skipValidation = false;
-
-    public function updateWaktu($id_jawaban)
-    {
-        return $this->query('UPDATE jawaban SET updated = NOW() WHERE id_jawaban = ?', [$id_jawaban]);
+    // recording this model to table jawaban in database
+    try {
+      $result = $this->db->insert("jawaban", $this);
+      return $result;
+    } catch (Exception $e) {
+      throw new Exception("Unauthorized or Unregisted");
     }
+  }
 
-    public function listByIdPertanyaan($id_pertanyaan)
-    {
-        return $this->db->get_where("jawaban","id_pertanyaan = '$id_pertanyaan'")->result_object();
-    }
+  //  mengambil data yang baru masuk
+  public function newest_jawaban()
+  {
+    $newest = $this->db->query("SELECT * FROM `jawaban` ORDER BY no DESC LIMIT 1")->result();
+    return $newest;
+  }
 
-    // public function listById($id_pertanyaan)
-    // {
-    //     return $this->query('SELECT a.id AS id_jawaban, a.isi as b.id AS id_balasan, b.isi AS isi_balasan, b.nama AS FROM jawaban a
-    //     LEFT JOIN balasan b ON a.id = b.id_jawaban
-    //     WHERE a.id_pertanyaan = ?
-    //     ORDER BY a.created, b.created', [$id_pertanyaan])->getResult();
-    // }
+  // mengambil jawaban berdasarkan id user
+  public function getAllByUser($id){
+    return $this->db->get_where("jawaban","id = $id")->result_object();
+  }
 
-    public function listByPertanyaanIds($pertanyaanIds)
-    {
-        return $this->db->query('SELECT a.* FROM jawaban a
-        WHERE a.id_pertanyaan IN ?
-        ORDER BY a.created_at', [$pertanyaanIds])->result();
-    }
+  // mengambil semua jawaban
+  public function all_jawaban()
+  {
+    $all = $this->db->get("jawaban")->result();
+    return $all;
+  }
 
-    public function delete_jawaban($id_jawaban)
-    {
-      $this->db->where('id', $id_jawaban);
-      return $this->db->delete('jawaban');
-    }
+  // mengambil jawaban by id
+  public function jawabanById($id)
+  {
+    $jawaban = $this->db->get_where("jawaban", "id_jawaban = '$id'")->result();
+    return $jawaban;
+  }
+
+  public function delete_jawaban($id_jawaban)
+  {
+    $this->db->where('id', $id_jawaban);
+    return $this->db->delete('jawaban');
+  }
+
+  public function upvoteJawaban($id)
+  {
+    // UPDATE jawaban SET upvote='2' WHERE id_jawaban=1;
+  }
 }
